8abf6dce697015c9b35244b718c9ee4b156c138b
Vasily Styagov <styagov.dev@gmail.com>
2026-05-21 19:21:09 +0100

    test: add acl tests

---
+++ integration-tests/tests/access_control.rs
@@ -0,0 +1,276 @@
+use serde_json::json;
+use tracing::info;
+
+mod common;
+use common::{panic::PanicFinder, prepare::Context};
+
+#[tokio::test]
+#[tracing::instrument]
+async fn test_acl_grant_role() -> anyhow::Result<()> {
+    let context = Context::builder().with_bob().build().await?;
+
+    info!("call acl_grant_role(Oracle, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("view acl_has_role(Oracle, alice)");
+    let has_role: bool = context
+        .sweat
+        .view("acl_has_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.alice.id(),
+        }))
+        .await?
+        .json()?;
+    assert!(has_role);
+    info!("acl_has_role: {has_role:?}");
+
+    info!("call acl_grant_role(Oracle, bob) [signer=alice, unauthorized]");
+    let granted: Option<bool> = context
+        .alice
+        .call(context.sweat.id(), "acl_grant_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.bob().id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, None);
+    info!("acl_grant_role: {granted:?}");
+
+    info!("view acl_has_role(Oracle, bob)");
+    let has_role: bool = context
+        .sweat
+        .view("acl_has_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.bob().id(),
+        }))
+        .await?
+        .json()?;
+    assert!(!has_role);
+    info!("acl_has_role: {has_role:?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+#[tracing::instrument]
+async fn test_acl_record_batch() -> anyhow::Result<()> {
+    let context = Context::builder().build().await?;
+
+    info!("call record_batch([(alice, 10_000)]) [signer=alice, unauthorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "record_batch")
+        .args_json(json!({ "steps_batch": [[context.alice.id(), 10_000]] }))
+        .transact()
+        .await?
+        .into_result();
+    assert!(result.has_panic("Insufficient permissions for method record_batch restricted by access control."));
+    info!("record_batch: {result:?}");
+
+    info!("call acl_grant_role(Oracle, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("call record_batch([(alice, 10_000)]) [signer=alice, authorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "record_batch")
+        .args_json(json!({ "steps_batch": [[context.alice.id(), 10_000]] }))
+        .transact()
+        .await?
+        .into_result()?;
+    assert!(result.outcome().is_success());
+    info!("record_batch: {result:?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+#[tracing::instrument]
+async fn test_acl_defer_batch() -> anyhow::Result<()> {
+    let context = Context::builder().with_claim().build().await?;
+
+    info!("call defer_batch([(alice, 10_000)], holding=claim) [signer=alice, unauthorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "defer_batch")
+        .args_json(json!({
+            "steps_batch": [[context.alice.id(), 10_000]],
+            "holding_account_id": context.claim().id(),
+        }))
+        .max_gas()
+        .transact()
+        .await?
+        .into_result();
+    assert!(result.has_panic("Insufficient permissions for method defer_batch restricted by access control."));
+    info!("defer_batch: {result:?}");
+
+    info!("call acl_grant_role(Oracle, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "Oracle",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("call defer_batch([(alice, 10_000)], holding=claim) [signer=alice, authorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "defer_batch")
+        .args_json(json!({
+            "steps_batch": [[context.alice.id(), 10_000]],
+            "holding_account_id": context.claim().id(),
+        }))
+        .max_gas()
+        .transact()
+        .await?
+        .into_result()?;
+    assert!(result.outcome().is_success());
+    info!("defer_batch: {result:?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+#[tracing::instrument]
+async fn test_acl_pause_feature() -> anyhow::Result<()> {
+    let context = Context::builder().build().await?;
+
+    info!("call pa_pause_feature(ALL) [signer=alice, unauthorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "pa_pause_feature")
+        .args_json(json!({ "key": "ALL" }))
+        .transact()
+        .await?
+        .into_result();
+    assert!(result.has_panic("Insufficient permissions for method pa_pause_feature restricted by access control."));
+    info!("pa_pause_feature: {result:?}");
+
+    info!("call acl_grant_role(PauseManager, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "PauseManager",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("call pa_pause_feature(ALL) [signer=alice, authorized]");
+    let is_paused: bool = context
+        .alice
+        .call(context.sweat.id(), "pa_pause_feature")
+        .args_json(json!({ "key": "ALL" }))
+        .transact()
+        .await?
+        .json()?;
+    assert!(is_paused);
+    info!("pa_pause_feature: {is_paused:?}");
+
+    Ok(())
+}
+
+#[tokio::test]
+#[tracing::instrument]
+async fn test_acl_unpause_feature() -> anyhow::Result<()> {
+    let context = Context::builder().build().await?;
+
+    info!("call pa_unpause_feature(ALL) [signer=alice, unauthorized]");
+    let result = context
+        .alice
+        .call(context.sweat.id(), "pa_unpause_feature")
+        .args_json(json!({ "key": "ALL" }))
+        .transact()
+        .await?
+        .into_result();
+    assert!(result.has_panic("Insufficient permissions for method pa_unpause_feature restricted by access control."));
+    info!("pa_unpause_feature: {result:?}");
+
+    info!("call acl_grant_role(PauseManager, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "PauseManager",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("call acl_grant_role(UnpauseManager, alice) [signer=contract, super-admin]");
+    let granted: Option<bool> = context
+        .sweat
+        .call("acl_grant_role")
+        .args_json(json!({
+            "role": "UnpauseManager",
+            "account_id": context.alice.id(),
+        }))
+        .transact()
+        .await?
+        .json()?;
+    assert_eq!(granted, Some(true));
+    info!("acl_grant_role: {granted:?}");
+
+    info!("call pa_pause_feature(ALL) [signer=alice, authorized]");
+    let is_paused: bool = context
+        .alice
+        .call(context.sweat.id(), "pa_pause_feature")
+        .args_json(json!({ "key": "ALL" }))
+        .transact()
+        .await?
+        .json()?;
+    assert!(is_paused);
+    info!("pa_pause_feature: {is_paused:?}");
+
+    info!("call pa_unpause_feature(ALL) [signer=alice, authorized]");
+    let is_unpaused: bool = context
+        .alice
+        .call(context.sweat.id(), "pa_unpause_feature")
+        .args_json(json!({ "key": "ALL" }))
+        .transact()
+        .await?
+        .json()?;
+    assert!(is_unpaused);
+    info!("pa_unpause_feature: {is_unpaused:?}");
+
+    Ok(())
+}
